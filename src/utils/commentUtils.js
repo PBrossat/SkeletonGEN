3cde713d4ab3a3bed0b404a3cadbef6513f78382
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-29 22:58:30 +0100

    Utils
    Fonction utile permettant de verifier si la ligne en parametre de la fonction est un commentaire ou non, cette fonction sera utilisée par d'autres fonctions dans d'autres fichiers

---
+++ src/utils/commentUtils.js
@@ -0,0 +1,13 @@
+/**
+ * Check if the line in parameter is a comment (c++ or c style)
+ * 
+ * @param {string} lineText
+ * @returns {boolean} - True if the line is a comment, false otherwise.
+ */
+function isComment(lineText) {
+    return lineText.trim().startsWith('//') || lineText.trim().startsWith('/*') || lineText.trim().startsWith('*');
+  }
+  
+module.exports = {
+    isComment,
+};
