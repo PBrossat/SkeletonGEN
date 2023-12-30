44d5fb660a73be3a30d42523336a5abf1fe88eab
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-30 18:11:34 +0100

    Maj de la fonction getAllMethodsWithSignature
    Utilisation d'un parseur, verification qu'une ligne n'est pas en commentaire ou inline et exctraction du type de retour, nom de la methode, parametres.

--- src/Methods.js
+++ src/Methods.js
@@ -1,42 +1,64 @@
 // eslint-disable-next-line no-unused-vars
 const vscode = require("vscode");
+const { isComment } = require("./utils/commentUtil");
 
-//TODO : Si dans un commentaire, ne pas prendre en compte
+
+
 
 /**
- * Extracts and parses method signatures from a C++ file (.h).
+ * Extracts and parses method signatures from a C++ file (.h). 
+ * It ignores inline methods and comments methods.
  *
  * @param {vscode.TextDocument} file - The TextDocument representing the C++ file.
  * @returns {Array<{ returnType: string, methodName: string, parameters: string }>} - An array of objects representing method signatures, each containing returnType, methodName, and parameters.
  */
 function getAllMethodsWithSignature(file) {
-  const fileContent = file.getText();
-
-  const methods = fileContent.match(
-    /\b\s*(?:inline\s+)?\s*[a-zA-Z0-9_]+\s+[a-zA-Z0-9_]+\s*\([^\)]*\)/g
-  ); // [return type] [method name] ([parameters])
+  const lineCount = file.lineCount;
 
   const result = [];
 
-  // Create a template array with : return type, method name, parameters
-  for (let i = 0; i < methods.length; i++) {
-    // If the method is inline, skip it
-    if (/\binline\b/.test(methods[i])) {
+  // Browse the file line by line
+  for (let lineIndex = 0; lineIndex < lineCount; lineIndex++) {
+    const lineText = file.lineAt(lineIndex).text;
+
+    // Ignore comments line (c++ or c style)
+    if (isComment(lineText)) {
       continue;
     }
-    const returnType = methods[i].match(/[a-zA-Z0-9_]+/)[0]; // get the return type
-    const methodName = methods[i].split(" ")[1].split("(")[0]; // get the method name
-    const parameters = methods[i].match(/\([^\)]*\)/)[0]; // get the parameters (if exist)
 
-    result.push({
-      returnType,
-      methodName,
-      parameters,
-    });
+    // If the method is found ([return type] [method name] ([parameters]))
+    const methodRegex = new RegExp(/\b\s*(?:inline\s+)?\s*[a-zA-Z0-9_]+\s+[a-zA-Z0-9_]+\s*\([^\)]*\)/g);
+
+    const matchResult = lineText.match(methodRegex);
+
+    if (matchResult) {
+      // If it's inline, skip it
+      if (/\binline\b/.test(lineText)) {
+        continue;
+      }
+
+      const returnType = matchResult[0].match(/[a-zA-Z0-9_]+/)[0]; // get the return type
+      const methodName = matchResult[0].split(" ")[1].split("(")[0]; // get the method name
+      const parameters = matchResult[0].match(/\([^\)]*\)/)[0]; // get the parameters (if exist)
+
+      result.push({
+        returnType,
+        methodName,
+        parameters,
+      });
+    }
   }
 
   return result;
 }
+
+
+
+
+
+
+
+
 
 /**
  * Extracts the method implementation from the C++ file (.cpp) if it already exists else, creates a skeleton.
@@ -49,18 +71,11 @@ function getAllMethodsWithSignature(file) {
 function createMethodSkeleton(file, method, className) {
   let methodSkeleton = "";
 
-  const parameters = method.parameters.replace(/([()])/g, "\\$1"); // Escape the parentheses
-
-  // Create a regex to find the method implementation
-  const methodRegex = new RegExp(
-    `(${method.returnType}\\s*${className}::${method.methodName}\\s*${parameters}\\s*{[^}]*})`
-  );
-
-  const methodAlreadyImplemented = file.getText().match(methodRegex);
+  const isMethodAlreadyImplemented = MethodAlreadyImplemented(file, method, className);
 
   // If the method is already implemented, we keep the implementation and append it to the skeleton
-  if (methodAlreadyImplemented) {
-    methodSkeleton = `${methodAlreadyImplemented[1]}\n\n`;
+  if (isMethodAlreadyImplemented) {
+    methodSkeleton = `${isMethodAlreadyImplemented[1]}\n\n`;
   }
   // If the method is not implemented, we create a skeleton
   else {
@@ -73,18 +88,28 @@ function createMethodSkeleton(file, method, className) {
 }
 
 /**
- *
- * @param {vscode.TextDocument} file - The TextDocument representing the C++ file.
- * @returns {string} - The name of the class.
+ * Method to check if a method is already implemented in the C++ file (.cpp).
+ * 
+ * @param {vscode.TextDocument} file - The TextDocument representing the C++ file (definition file).
+ * @param {{ returnType: string, methodName: string, parameters: string }} method - An object representing a method signature, containing returnType, methodName, and parameters.
+ * @param {string} className - The name of the main class.
+ * @returns {Array} - An array containing the method implementation if it exists, null otherwise.
  */
-function getClassName(file) {
-  const fileContent = file.getText();
-  const className = fileContent.match(/class\s+([a-zA-Z0-9_]+)/)[1]; // class [space] name
-  return className;
+function MethodAlreadyImplemented(file, method, className) {
+  const parameters = method.parameters.replace(/([()])/g, "\\$1"); // Escape the parentheses
+
+  // Create a regex to find the method implementation
+  const methodRegex = new RegExp(
+    `(${method.returnType}\\s*${className}::${method.methodName}\\s*${parameters}\\s*{[^}]*})`
+  );
+
+  const methodAlreadyImplemented = file.getText().match(methodRegex);
+
+  return methodAlreadyImplemented; 
 }
+
 
 module.exports = {
   getAllMethodsWithSignature,
-  getClassName,
   createMethodSkeleton,
 };
