ec051a7aa7fde17c0fbc6cbc9b8a8a50340833a9
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-01-04 21:58:42 +0100

    Add fichier
    Ajout du fichier browseFile.js et sa fonction browseFileToGetImplementation utilisée dans toute les création de squelettes. Fonction qui permet de ne pas ré-implémenter à chaque fois la meme fonction dans tous les fichiers

---
+++ src/utils/browserFile.js
@@ -0,0 +1,87 @@
+// eslint-disable-next-line no-unused-vars
+const vscode = require("vscode");
+const { isCommentLine, updateFlagIsBlockComment } = require("./commentUtil");
+
+/**
+ * Method to check if the method, constructor or destructor is already implemented in the file (cpp file) and extract the implementation (if it exists)
+ *
+ * @param {vscode.TextDocument} file - The file we want to browse
+ * @param {string} className - The name of the main class
+ * @param {RegExp} regex - The regex to match the line we want to find
+ * @returns {string[]} An array containing the implementation if it exists, empty array otherwise
+ */
+function browseFileToGetImplementation(file, className, regex) {
+  const lineCount = file.lineCount;
+  const result = [];
+  let isBlockComment = false;
+
+  // Browse the file line by line
+  for (let lineIndex = 0; lineIndex < lineCount; lineIndex++) {
+    const lineText = file.lineAt(lineIndex).text;
+
+    // Update the flag isBlockComment if the line contains /* or */
+    isBlockComment = updateFlagIsBlockComment(lineText, isBlockComment);
+
+    // if the line match the regex in parameter
+    if (lineText.match(regex)) {
+      if (isBlockComment) {
+        result.push("/*");
+      }
+
+      // If the line contains {, increment the number of open brackets
+      let numberOfOpenBrackets = lineText.includes("{") ? 1 : 0;
+
+      result.push(lineText);
+
+      // Browse the file line by line starting from the next line
+      for (
+        let nextLineIndex = lineIndex + 1;
+        nextLineIndex < lineCount;
+        nextLineIndex++
+      ) {
+        const currentLineText = file.lineAt(nextLineIndex).text;
+
+        isBlockComment = updateFlagIsBlockComment(
+          currentLineText,
+          isBlockComment
+        );
+
+        // If the line is on a comment, we increment the number of lines of the constructor implementation and add the comment line to the result
+        if (isCommentLine(currentLineText)) {
+          result.push(currentLineText);
+          continue;
+        }
+
+        // If the line contains {, increment the number of open brackets
+        if (currentLineText.includes("{")) {
+          numberOfOpenBrackets++;
+        }
+
+        // If the line contains }, decrement the number of open brackets
+        if (currentLineText.includes("}")) {
+          numberOfOpenBrackets--;
+        }
+
+        // The constructor implementation is finished
+        if (numberOfOpenBrackets === 0) {
+          result.push(currentLineText);
+
+          // If the method implementation is in a block comment, we add the closing comment tag
+          if (isBlockComment) {
+            result.push("*/");
+          }
+
+          return result; // Not necessary to browse the rest of the file
+        }
+
+        // We continue to browse the file
+        result.push(currentLineText);
+      }
+    }
+  }
+  return result;
+}
+
+module.exports = {
+  browseFileToGetImplementation,
+};
